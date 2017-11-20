4576c993770685dd9fdf716e237421c2024677fe
peter <peter@vaadin.com>
2017-11-20 21:44:14 +0200

    Entry point

---
+++ src/main/java/com/vaadin/peter/foundation/app/FoundationApplication.java
@@ -0,0 +1,12 @@
+package com.vaadin.peter.foundation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(FoundationApplication.class, args);
+	}
+}
