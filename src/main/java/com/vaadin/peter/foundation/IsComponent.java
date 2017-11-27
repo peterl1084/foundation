b58d27aa2c1dbf127c2210e94be02b8e365d34bd
peter <peter@vaadin.com>
2017-11-27 23:02:56 +0200

    IsComponent interface comments

--- src/main/java/com/vaadin/peter/foundation/IsComponent.java
+++ src/main/java/com/vaadin/peter/foundation/IsComponent.java
@@ -2,8 +2,21 @@ package com.vaadin.peter.foundation;
 
 import com.vaadin.ui.Component;
 
+/**
+ * {@link IsComponent} is marker interface for any such type that internally is
+ * Vaadin component but not exposing traditional {@link Component} API through
+ * the main public API.
+ * 
+ * @author Peter / Vaadin
+ *
+ * @param <C>
+ *          type of the component this type represents.
+ */
 public interface IsComponent<C extends Component> {
 
+  /**
+   * @return this as Vaadin {@link Component}
+   */
   @SuppressWarnings("unchecked")
   default C asComponent() {
     return (C) this;
